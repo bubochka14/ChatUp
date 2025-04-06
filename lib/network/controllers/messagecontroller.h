8ee8815e06cadc1031510e12120ab3c498ccbef9
Denis Istomin <rex.hd.kanal@gmail.com>
2025-04-06 12:37:20 +0300

    fix races in message model and controller, add locking methods

--- lib/network/controllers/messagecontroller.h
+++ lib/network/controllers/messagecontroller.h
@@ -20,11 +20,9 @@ namespace Message {
 		explicit Controller(QObject* parent = nullptr);
 		Q_INVOKABLE virtual Model* model(int roomID) = 0;
 		Q_INVOKABLE virtual QFuture<void> updateMessageBody(const QString& body, int roomID) = 0;
-		Q_INVOKABLE virtual QFuture<void> create(const QString& body, int roomID) = 0;
+		Q_INVOKABLE virtual QFuture<int> create(const QString& body, int roomID) = 0;
 		Q_INVOKABLE virtual QFuture<void> remove(int roomid, int messId) = 0;
-		Q_INVOKABLE virtual QFuture<void> markAsRead(int roomID, size_t count) = 0;
 		Q_INVOKABLE virtual QFuture<void> load(int roomID, int row, int fromIndex, int  toIndex) =0;
-
 
 	};
 	class CC_NETWORK_EXPORT CallerController : public Controller
@@ -36,14 +34,10 @@ namespace Message {
 		Model* model(int roomID) override;
 		QFuture<void> load(int roomID,int row, int fromIndex, int  toIndex) override;
 		QFuture<void> updateMessageBody(const QString& body, int roomID) override;
-		QFuture<void> create(const QString& body, int roomId) override;
-		QFuture<void> remove(int roomid, int messId) override;
-		QFuture<void> markAsRead(int roomID, size_t count) override;
+		QFuture<int> create(const QString& body, int roomID) override;
+		QFuture<void> remove(int roomID, int messID) override;
 		QFuture<void> initialize() override;
 		void reset() override;
-
-	protected:
-		void connectToDispatcher();
 	private:
 		std::shared_ptr<NetworkCoordinator> _manager;
 		std::unordered_map<int, Model*> _history;
